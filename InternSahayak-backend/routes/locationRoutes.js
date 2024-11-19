066c038d6fb951a95587cf9f9feffc7e8fe4c137
KRISH bhagat <93695269+apniprogrammingyt09@users.noreply.github.com>
2024-11-20 00:24:03 +0530

    Delete InternSahayak-backend directory

--- InternSahayak-backend/routes/locationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getLocationsByStudentId, createLocation } = require('../controllers/locationController');
-
-const router = express.Router();
-
-router.get('/:student_id', getLocationsByStudentId); // Get locations for a specific student
-router.post('/', createLocation); // Create a location
-
-module.exports = router;
